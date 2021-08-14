a9b0c23e636123db7a50529004b447d75f23d14c
ljourm <ljourm@gmail.com>
2021-08-15 05:13:36 +0900

    fix: configファイルが空だった場合のエラー対応

--- src/lib/app/config.rb
+++ src/lib/app/config.rb
@@ -9,8 +9,8 @@ module App
       def config
         @config unless @config.nil?
 
-        application_config = YAML.load_file(application_config_filename)
-        environment_config = YAML.load_file(environment_config_filename)
+        application_config = YAML.load_file(application_config_filename) || {}
+        environment_config = YAML.load_file(environment_config_filename) || {}
 
         @config = Utility.transform_keys_to_sym(application_config.merge(environment_config))
       end
